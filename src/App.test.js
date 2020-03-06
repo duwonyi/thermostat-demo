08db15577b2a70f90495b25cbb2ca32c46578c5a
Doowon Lee <doowon.lee@amdocs.com>
2020-03-06 18:49:42 -0500

    clean up

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/Parity Thermostat/i)
+  const linkElement = getByText(/ROOM/i)
   expect(linkElement).toBeInTheDocument()
 })
